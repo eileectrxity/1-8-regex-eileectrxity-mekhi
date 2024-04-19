2aeb1a9567f6d0f42b9afaaba23eff62de8722d3
electrxety <er191712@gmail.com>
2024-04-18 22:53:28 -0400

    solved question 18

--- src/debug.js
+++ src/debug.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars, no-useless-escape */
 
+//QUESTION 17: debugged
 const isValidCompanyUsername = (username, firstName, lastName) => {
   const regex = new RegExp(
     `^(sales|tech)\\d\\w-${firstName[0]}${lastName}\\d?$`
